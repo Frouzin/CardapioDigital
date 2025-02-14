e21eaa2e3b26871b3a26e57337c0c3a196436e30
Frouzin <matheusereis44@gmail.com>
2025-02-13 21:26:17 -0300

    Back att

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
 global.config = require('./config').get('dev');
+
+const restify = require("restify");
+const path = require("path");
+const recursiveReaddir = require("recursive-readdir");
+
+//Cria o servidor
+const server = restify.createServer({
+    name: 'Delivery',
+    version: '1.0'
+});
+
