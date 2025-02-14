70637659191a51e0c3c15a9d3744adeabe8cd1e9
Frouzin <matheusereis44@gmail.com>
2025-02-13 22:17:44 -0300

    atualizações no back end e bug fix in index

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
-global.config = require('./config').get('dev');
+// Carrega a configuração do arquivo e a atribui a uma variável
+const appConfig = require('./config').get('dev');
+
+// (Opcional) Define a configuração como global, se necessário
+global.config = appConfig;
 
 const restify = require("restify");
 const path = require("path");
 const recursiveReaddir = require("recursive-readdir");
-const { config } = require('process');
 
-//Cria o servidor
+// Cria o servidor
 const server = restify.createServer({
     name: 'Delivery',
     version: '1.0'
@@ -17,35 +20,31 @@ server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 server.use(restify.plugins.urlEncodedBodyParser());
 
-//Adiciona todas as rotas na inicialização do server. busca na pasta "Routes"
+// Adiciona todas as rotas na inicialização do server. Busca na pasta "Routes"
 const pathFiles = path.resolve(path.resolve('./').concat('/server/routes'));
 
-recursiveReaddir(pathFiles, ['!*.js'],(err, files) => {
+recursiveReaddir(pathFiles, ['!*.js'], (err, files) => {
     if(err){
         console.log(err);
         process.exit(1);
     }
-    files.forEach(element => { require(element)(server) })
+    files.forEach(element => { require(element)(server) });
 });
 
-// Utilizado para não ter problema com requisições no Chrome(CORS)
-server.use(
-    function nocache(req, res, next){
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.header("Pragma", "no-cache");
-        next();
-    }
-)
+// Utilizado para não ter problema com requisições no Chrome (CORS)
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Pragma", "no-cache");
+    next();
+});
 
-//Modifica o erro e mostra pro usuario uma mensagem personalizada
-server.on('restifyError',function(req, res, err, callback){
+// Modifica o erro e mostra pro usuário uma mensagem personalizada
+server.on('restifyError', (req, res, err, callback) => {
     err.toJSON = function customToJSON(){
-        return{
-            Erro: 'Página não encontrada'
-        }
+        return { Erro: 'Página não encontrada' };
     };
     return callback();
 });
 
-module.exports = Object.assign({ server,restify, config });
+module.exports = { server, restify, config: appConfig };
