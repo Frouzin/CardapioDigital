ac76695c26e6d307628ebe4d0bff225199b4b15e
Frouzin <matheusereis44@gmail.com>
2025-03-17 01:19:59 -0300

    metodo de login e validação de campos

--- client/painel/js/login.js
+++ client/painel/js/login.js
@@ -7,6 +7,9 @@ var login = {};
 login.event = {
 
     init:() => {
+        document.querySelector("#btnLogin").onclick = () => {
+            login.method.validarLogin();
+        }
 
     }
 
@@ -14,5 +17,43 @@ login.event = {
 
 login.method = {
 
+    //Valida os campos
+    validarLogin : () => {
+        let email = document.querySelector('#txtEmailLogin').value.trim();
+        let senha = document.querySelector('#txtSenhaLogin').value.trim();
+
+        if(email.length == 0){
+            alert("Informe o Email, por favor.");
+            document.querySelector("#txtEmailLogin").focus();
+            return;
+        }
+
+        if(senha.length == 0){
+            alert("Informe a Senha, por favor.");
+            document.querySelector("#txtSenhaLogin").focus();
+            return;
+        }
+
+        login.method.login(email,senha);
+
+    },
+
+    //metodo que gaz o login (via API)
+    login: (email, senha) => {
+
+        var dados = {
+            email:email,
+            senha:senha
+        }
+        app.method.post('/login', JSON.stringify(),
+            (response) => {
+                console.log(response);
+            },
+            (error) => {
+                console.log(error);
+
+            }    
+        )
+    }
 
 }
